809ab6ae24e9511264812fd8db4437204046c1f0
Pankaj <pankajyadavpy069@gmail.com>
2025-01-02 19:06:28 +0530

    ES6 Modules and Common JS

---
+++ jsFoundation/part9_adv/appC.js
@@ -0,0 +1,5 @@
+const mathOperations = require("./mathOperationsC.js");
+
+console.log(mathOperations.add(2, 2));
+console.log(mathOperations.subtract(5, 2));
+console.log(mathOperations.multiply(3, 2));
---
+++ jsFoundation/part9_adv/appM.js
@@ -0,0 +1,17 @@
+// Module Import 
+
+
+// default import
+
+import multiply from "./mathOperationsM.js";
+
+// named import
+
+import {dd, subtract} from "./mathOperationsM.js";
+
+console.log(multiply(3, 5));
+
+console.log(add(2, 2));
+
+
+
